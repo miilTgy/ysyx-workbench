3a59705573f08538d7faddc9cd7689de0e9bf476
tracer-ysyx <tracer@ysyx.org>
2024-02-04 21:51:19 +0800

    Add Feature(struct): si (Single Step Execution).

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -63,6 +63,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Single-step excution si [N], N is steps default 1"}
 
   /* TODO: Add more commands */
 
