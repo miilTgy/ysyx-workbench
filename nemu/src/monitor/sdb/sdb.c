9f1caf22954e558fc251e27e997a4fe57271e96b
Zeng GuangYi <tgy_scut2021@outlook.com>
2024-02-05 01:10:00 +0800

    Add Feature(struct): info (Show register or watchpoint info).

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -69,6 +69,10 @@ static int cmd_si(char *args) {
   return 0;
 }
 
+static int cmd_info(char *args) {
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -80,7 +84,7 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", "Single-step excution si [N], N is No. of steps default 1", cmd_si},
-
+  { "info", "Show register value OR Show watch point info", cmd_info},
   /* TODO: Add more commands */
 
 };
