f81c7708792a072f69c55cce801c677cc1c1358b
Zeng GuangYi <tgy_scut2021@outlook.com>
2024-02-05 03:48:40 +0800

    Fixed Bug: info r can't handel null target(generate core dump).

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -71,6 +71,10 @@ static int cmd_si(char *args) {
 
 static int cmd_info(char *args) {
   char *target = strtok(NULL, " ");
+  if (target == NULL) {
+    printf("ERROR: missing info target: r/w\n");
+    return 0;
+  }
   printf("target=%s\n", target);
   if (strcmp(target, "r") == 0) {
     isa_reg_display();
