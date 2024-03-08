223233d4bbbc85fd9d581320da35dd20ec386aca
tracer-ysyx <tracer@ysyx.org>
2024-03-08 23:14:40 +0800

    > compile NEMU ysyx_22040000 曾广翼 Linux mPC 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:40 up  1:52,  1 user,  load average: 0.88, 0.75, 0.69

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -142,6 +142,7 @@ static bool make_token(char *e) {
         break;
       }
     }
+    printf("nr of tokens: %d\n", nr_token);
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
@@ -160,7 +161,7 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  printf("value: %ld", eval(0, nr_token));
+  printf("value: %ld", eval(0, nr_token-1));
 
   return 0;
 }
