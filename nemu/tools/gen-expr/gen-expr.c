1e8781836d173c4c948dcf86645104ff144f210f
Zeng GuangYi <tgy_scut2021@outlook.com>
2024-03-16 16:08:10 +0800

    Fixed Bug: program quit when expr oversize.

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -70,7 +70,10 @@ void gen_div() {
 static void gen_rand_expr() {
   if (buf_indx >= 65536) {
     printf("\033[0m\033[1;31m%s\033[0m\n", "Expression Oversize!");
-    exit(1);
+    buf_indx = 0;
+    gen_rand_expr();
+    buf[buf_indx++] = '\0';
+    // exit(1);
   }
   switch (choose(3)) {
     case 0: gen_num(); break;
