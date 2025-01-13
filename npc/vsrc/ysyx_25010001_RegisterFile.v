7959ba30bd6b2da01c6d70bbff4f6512e3d9756d
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-01-14 00:42:29 +0800

    Functionalize read port of RegisterFile.

--- npc/vsrc/ysyx_25010001_RegisterFile.v
+++ npc/vsrc/ysyx_25010001_RegisterFile.v
@@ -1,13 +1,15 @@
-module ysyx_25010001_RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
+module ysyx_25010001_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 64) (
   input clk,
-  input [DATA_WIDTH-1:0] rdata,
-  input [ADDR_WIDTH-1:0] raddr,
   input [DATA_WIDTH-1:0] wdata,
   input [ADDR_WIDTH-1:0] waddr,
-  input wen
+  input wen,
+  input [ADDR_WIDTH-1:0] raddr,
+  output [DATA_WIDTH-1:0] rdata
 );
   reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
   always @(posedge clk) begin
     if (wen) rf[waddr] <= wdata;
   end
+
+  assign rdata = (raddr == 0) ? 0 : rf[raddr];
 endmodule
