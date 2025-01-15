297e91ca78292f6138d0803ebd7b5da1ced4b131
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-01-16 02:27:05 +0800

    Add check_eq and some macro for tb.

--- npc/csrc/tb_RegisterFile.cpp
+++ npc/csrc/tb_RegisterFile.cpp
@@ -1,35 +1,40 @@
 #include "tb_common.h"
 #include "Vysyx_25010001_RegisterFile.h"
 
-#define set_write(tb, addr, data) do { \
-    tb->dut->wen = 1; \
-    tb->dut->waddr = addr; \
-    tb->dut->wdata = data; \
+
+#define set_write(addr, data) do { \
+    TB(DUT(wen)) = 1; \
+    TB(DUT(waddr)) = addr; \
+    TB(DUT(wdata)) = data; \
 } while (0)
 
-#define set_read(tb, addr) do { \
-    tb->dut->raddr = addr; \
-} while(0)
+#define set_read(addr) do { \
+    TB(DUT(raddr)) = addr; \
+} while( 0)
 
-
-TESTBENCH<Vysyx_25010001_RegisterFile> *tb;
+TESTBENCH<Vysyx_25010001_RegisterFile> *__TB__;
 
 int main(int argc, char *argv[]) {
-    tb = new TESTBENCH<Vysyx_25010001_RegisterFile>(argc, argv);
+    __TB__ = new TESTBENCH<Vysyx_25010001_RegisterFile>(argc, argv, sequential);
+    TB(sim_init());
 
-    tb->sim_init();
-    tb->dut->clk = 1;
-    tb->sim_cycles(3);
+    TB(cycles(3));
     int last = 0;
-    for (int i=0; i<32; i++) {
-        step(tb, {
-            set_write(tb, i, tb->rand64()); set_read(tb, last);
+    vluint64_t last_data = 0;
+    for (int i=1; i<32; i++) {
+        vluint64_t tmp = TB(rand64());
+        vluint64_t read_data = 0;
+        step({
+            set_write(i, tmp); set_read(last);
         });
+        std::cout << i << std::endl;
+        read_data = TB(DUT(rdata));
+        TB(check_eq(last_data, read_data));
+        last_data = tmp;
         last = i;
     }
-    tb->sim_cycles(10);
+    TB(cycles(10));
 
-    tb->~TESTBENCH();
-    std::cout << "sim finished." << std::endl;
-    exit(EXIT_SUCCESS);
+    TB(~TESTBENCH());
+    exit(EXIT_SUCCESS);    
 }
--- npc/csrc/tb_common.h
+++ npc/csrc/tb_common.h
@@ -2,14 +2,17 @@
  * @Author: Zeng GuangYi tgy_scut2021@outlook.com
  * @Date: 2025-01-15 20:31:21
  * @LastEditors: Zeng GuangYi tgy_scut2021@outlook.com
- * @LastEditTime: 2025-01-15 23:52:11
+ * @LastEditTime: 2025-01-16 01:45:54
  * @FilePath: /npc/csrc/tb_common.h
  * @Description: Common Verilator testbench headder
  * 
  * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
  */
+
 #ifndef __TB_COM__ // dependency defender
 #define __TB_COM__
+
+#define NDEBUG
 
 #include <stdlib.h>
 #include <iostream>
@@ -18,18 +21,29 @@
 #include <typeinfo>
 #include <string>
 #include <random>
+#include <assert.h>
 
-#define step(tb, statements) do { \
-            tb->set_clk(0); \
-            tb->dut->eval(); \
-            tb->contextp->timeInc(1); \
-            tb->m_trace->dump(tb->contextp->time()); \
-            tb->set_clk(1); \
-            tb->dut->eval(); \
-            {statements} \
-            tb->dut->eval(); \
-            tb->contextp->timeInc(1); \
-            tb->m_trace->dump(tb->contextp->time()); \
+#define __TB__ tb
+#define __DUT__ dut
+
+#ifndef __TB__
+#error ERROR: NO __TB__ DEFINITION.
+#endif
+
+#define TB(a) __TB__->a
+#define DUT(a) __DUT__->a
+
+#define step(statements) do { \
+            TB(toggle_clk()); \
+            TB(__DUT__->eval()); \
+            TB(contextp->timeInc(1)); \
+            TB(m_trace->dump(TB(contextp->time()))); \
+                TB(toggle_clk()); \
+                TB(__DUT__->eval();) \
+                {statements} \
+                TB(__DUT__->eval()); \
+                TB(contextp->timeInc(1)); \
+                TB(m_trace->dump(TB(contextp->time()))); \
         } while (0)
 
 
@@ -42,28 +56,33 @@ private:
     Module_type module_type;
     
 public:
-    MODULE *dut;
+    MODULE *__DUT__;
     VerilatedContext *contextp;
     VerilatedVcdC *m_trace;
 
     /**
      * @description: Constructor:
-     *                  1. instantiate dut contextp m_trace
-     *                  2. set trace and trace file name.
-     * @param {int}   argc to Get initial value.
-     * @param {char} *argv to Get initial value.
+     *                  1. specify module type (seq or com).
+     *                  2. instantiate __DUT__ contextp m_trace
+     *                     with default value.
+     *                  3. set trace and trace file name.
+     * @param {int}         argc to Get initial value.
+     * @param {char}       *argv to Get initial value.
+     * @param {Module_type} module_type sepcify simulation type.
+     *                      (sequential or combinational?)
      * @return {*}
      */
-    TESTBENCH(int argc, char *argv[]) {
+    TESTBENCH(int argc, char *argv[], Module_type module_type) {
         std::cout << "start constructiog" << std::endl;
+        this->module_type = module_type;
         Verilated::traceEverOn(true);
         Verilated::commandArgs(argc, argv);
-        dut = new MODULE;
+        __DUT__ = new MODULE;
         contextp = new VerilatedContext;
         m_trace = new VerilatedVcdC;
 
         contextp->traceEverOn(true);
-        dut->trace(m_trace, 5);
+        __DUT__->trace(m_trace, 5);
         std::string module_name = typeid(MODULE).name();
         std::cout << module_name << std::endl;
         module_name += ".vcd";
@@ -77,19 +96,22 @@ public:
      * @return {*}
      */
     ~TESTBENCH(void) {
+        std::cout << "sim finished." << std::endl;
         m_trace->close();
         delete m_trace;
-        delete dut;
+        delete __DUT__;
         delete contextp;
     }
 
     /**
      * @description: Init sim, must called before any sim.
-     * @param {  } dut
      * @return {*}
      */
     void inline sim_init() {
-        dut->eval();
+        __DUT__->eval();
+        if (module_type == sequential) {
+            __DUT__->clk = 1;
+        }
         m_trace->dump(contextp->time());
     }
 
@@ -100,7 +122,17 @@ public:
      */
     void inline set_clk(CData i) {
         if (module_type == sequential) {
-            dut->clk = i;
+            __DUT__->clk = i;
+        }
+    }
+
+    /**
+     * @description: Verify clk and toggle clk.
+     * @return {*}
+     */
+    void inline toggle_clk() {
+        if (module_type == sequential) {
+            set_clk(!__DUT__->clk);
         }
     }
 
@@ -109,16 +141,18 @@ public:
      * @param {int} i # of cycles to sim.
      * @return {*}
      */
-    void inline sim_cycles(int i) {
+    void inline cycles(int i) {
         for (int j=0; j<i; j++) {
-            set_clk(0);
-            dut->eval();
+            toggle_clk();
+            __DUT__->eval();
             contextp->timeInc(1);
             m_trace->dump(contextp->time());
-            set_clk(1);
-            dut->eval();
-            contextp->timeInc(1);
-            m_trace->dump(contextp->time());
+            if (module_type == sequential) {
+                toggle_clk();
+                __DUT__->eval();
+                contextp->timeInc(1);
+                m_trace->dump(contextp->time());
+            }
         }
     }
 
@@ -131,6 +165,17 @@ public:
         uint32_t seed = rd();
         std::mt19937_64 rng(seed);
         return rng();
+    }
+
+    void inline check_eq(uint64_t a, uint64_t b) {
+        if (a != b) {
+            m_trace->close();
+            delete m_trace;
+            delete __DUT__;
+            delete contextp;
+            std::cout << "Assertion: a=" << a <<  "b=" << b << std::endl;
+            assert(0);
+        }
     }
 };
 
