7e460040d7607f87a4bfba767e315bb991314129
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-01 11:22:54 +0800

    Realize spintf and vsprintf in order to pass hello-str test.

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,6 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include <math.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -11,10 +12,52 @@ int printf(const char *fmt, ...) {
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
+  char *d = out, *s = (char *) fmt;
+  for (; *s != '\0'; s++) {
+    if (*s != '%') {
+      *d = *s;
+      d++;
+    } else {
+      s++;
+      switch (*s) {
+      case 's':
+        char *str = va_arg(ap, char *);
+        strcpy(d, str);
+        d += strlen(str);
+        break;
+      case 'd':
+        int num = va_arg(ap, int);
+        if (num < 0) {
+          *d = '-';
+          d++;
+          num = -num;
+        }
+        int cnt = 0, num_tmp = num;
+        while (num_tmp != 0) {
+          num_tmp /= 10;
+          cnt++;
+        }
+        for (size_t i = 0; i < cnt; i++) {
+          int digit = num % 10;
+          num /= 10;
+          d[cnt - i - 1] = 48 + digit;
+        }
+        d += cnt;
+        break;
+      default: break;
+      }
+    }
+  }
+  *d = '\0'; // Null-terminate the destination string
+  return d - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
