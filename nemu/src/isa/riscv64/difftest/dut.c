99471d3f9640cb8473fa79147286d61a2fd0d1b7
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-04 16:17:51 +0800

    Realize difftest.

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,7 +18,17 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (size_t i=0; i<32; i++) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+      printf("Diff error: reg %s: DUT = 0x%lx, REF = 0x%lx\n", reg_name(i), cpu.gpr[i], ref_r->gpr[i]);
+      return false;
+    }
+  }
+  if (cpu.pc != ref_r->pc) {
+    printf("Diff error: DUT pc = 0x%lx, REF pc = 0x%lx\n", cpu.pc, ref_r->pc);
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
