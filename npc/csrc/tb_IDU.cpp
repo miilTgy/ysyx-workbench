fb516e4cc0cf2efce90ad0e475036caead831ccf
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-07 11:02:32 +0800

    Add srai to test imm.

--- npc/csrc/tb_IDU.cpp
+++ npc/csrc/tb_IDU.cpp
@@ -9,6 +9,7 @@ static const uint32_t img [] = {
   0x0102c503,  // lbu   a0,16(t0)
   0xffc10113,  // addi  sp,sp,-4
   0x00100073,  // ebreak (used as nemu_trap)
+  0x40005013,  // srai x0, 0(x0)
   0xdeadbeef,  // some data
 };
 vluint64_t sim_pc = 0x80000000;
