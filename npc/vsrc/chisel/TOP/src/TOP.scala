d2a797110ee69d30ca99f2e9a4291b4703ee7097
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-07 19:43:07 +0800

    Move Main class into TOP.scala.

--- npc/vsrc/chisel/TOP/src/TOP.scala
+++ npc/vsrc/chisel/TOP/src/TOP.scala
@@ -24,3 +24,25 @@ class TOP extends Module {
     imem.io.pc := ifu.ioIMEM.pc
     io.inst_data := imem.io.inst_data
 }
+
+object Main extends App {
+    val firtoolOptions = Array(
+    "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+      "disallowLocalVariables",
+      "disallowPackedArrays",
+      "locationInfoStyle=wrapInAtSquareBracket"
+    ).reduce(_ + "," + _)
+    )
+    print("args: ")
+    for (i <- args) {
+      print(i + " ")
+    }
+    println()
+    println(
+      circt.stage.ChiselStage
+        .emitSystemVerilogFile(new top.TOP(), args :+ "--target-dir" :+ "vsrc/", firtoolOptions)
+    )
+}
+
