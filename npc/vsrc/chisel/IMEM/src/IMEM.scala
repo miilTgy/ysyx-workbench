1fb7801bdcdd1f3e848438bf914beffc50b34e6c
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-07 15:53:34 +0800

    Add IMEM.v resources for IMEM.scala.

--- npc/vsrc/chisel/IMEM/src/IMEM.scala
+++ npc/vsrc/chisel/IMEM/src/IMEM.scala
@@ -4,11 +4,11 @@ import chisel3._
 import chisel3.util.HasBlackBoxResource
 
 class IMEM extends BlackBox with HasBlackBoxResource {
-  val io = IO(new Bundle {
+    val io = IO(new Bundle {
     val pc = Input(UInt(64.W))
     val inst = Output(UInt(32.W))
-  })
+})
 
-  // Set the resource path for the Verilog file
-  addResource("/vsrc/chisel/IMEM/src/ysyx_25010001_IMEM.v")
+    // Set the resource path for the Verilog file
+    addResource("/IMEM.v")
 }
