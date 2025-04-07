4d9ae788d2436d7f9f09a07b347a51c038bbc9d4
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-07 18:26:36 +0800

    Create top module. - Create some simple connections.

---
+++ npc/vsrc/chisel/TOP/src/Main.scala
@@ -0,0 +1,22 @@
+import top.TOP
+
+object Main extends App {
+    val firtoolOptions = Array(
+    "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+      "disallowLocalVariables",
+      "disallowPackedArrays",
+      "locationInfoStyle=wrapInAtSquareBracket"
+    ).reduce(_ + "," + _)
+    )
+    print("args: ")
+    for (i <- args) {
+      print(i + " ")
+    }
+    println()
+    println(
+      circt.stage.ChiselStage
+        .emitSystemVerilogFile(new TOP(), args :+ "--target-dir" :+ "vsrc/", firtoolOptions)
+    )
+}
--- npc/vsrc/chisel/TOP/src/TOP.scala
+++ npc/vsrc/chisel/TOP/src/TOP.scala
@@ -4,12 +4,23 @@ import chisel3._
 
 import ifu.IFU
 import imem.IMEM
-import idu.IDU
-import alu.ALU
-import dmem.DMEM
-import wbu.WBU
+// import idu.IDU
+// import alu.ALU
+// import dmem.DMEM
+// import wbu.WBU
 
 class TOP extends Module {
-    
+    val io = IO(new Bundle {
+        val inst_data = Output(UInt(32.W))
+    })
 
+    val ifu = Module(new IFU())
+    val imem = Module(new IMEM())
+    // val idu = Module(new IDU())
+    // val alu = Module(new ALU())
+    // val dmem = Module(new DMEM())
+    // val wbu = Module(new WBU())
+
+    imem.io.pc := ifu.ioIMEM.pc
+    io.inst_data := imem.io.inst_data
 }
