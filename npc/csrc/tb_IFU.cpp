795a30b6937134c143449bc236eaae3da90c7f03
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-07 15:15:39 +0800

    Test the simple IFU.

--- npc/csrc/tb_IFU.cpp
+++ npc/csrc/tb_IFU.cpp
@@ -1,11 +1,11 @@
 #include "tb_common.h"
-#include "Vysyx_25010001_IFU.h"
+#include "VIFU.h"
 
 
-TESTBENCH<Vysyx_25010001_IFU> *__TB__;
+TESTBENCH<VIFU> *__TB__;
 
 int main(int argc, char *argv[]) {
-    __TB__ = new TESTBENCH<Vysyx_25010001_IFU>(argc, argv);
+    __TB__ = new TESTBENCH<VIFU>(argc, argv);
     TB(sim_init());
     TB(sim_reset());
     TB(cycles(10));
