4a18c155008d99ad6c639f6c2fdb06764315e921
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-08 13:50:32 +0800

    Add Missing ports for ysyx_25010001_RegisterFile.

--- npc/vsrc/ysyx_25010001_RegisterFile.v
+++ npc/vsrc/ysyx_25010001_RegisterFile.v
@@ -4,7 +4,9 @@ module ysyx_25010001_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 64) (
   input [ADDR_WIDTH-1:0] waddr,
   input wen,
   input [ADDR_WIDTH-1:0] raddr,
-  output [DATA_WIDTH-1:0] rdata
+  output [DATA_WIDTH-1:0] rdata,
+  input [ADDR_WIDTH-1:0] raddr2,
+  output [DATA_WIDTH-1:0] rdata2
 );
   reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
   always @(posedge clk) begin
@@ -12,4 +14,5 @@ module ysyx_25010001_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 64) (
   end
 
   assign rdata = (|raddr) ? rf[raddr] : 0;
+  assign rdata2 = (|raddr2) ? rf[raddr2] : 0;
 endmodule
