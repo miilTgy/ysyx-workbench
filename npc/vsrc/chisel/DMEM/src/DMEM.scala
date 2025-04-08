fd2ce1924e451f73f3063159b27873baf351d24c
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-08 13:47:44 +0800

    Fix DMEM IOs.

--- npc/vsrc/chisel/DMEM/src/DMEM.scala
+++ npc/vsrc/chisel/DMEM/src/DMEM.scala
@@ -5,8 +5,7 @@ import chisel3.util.HasBlackBoxResource
 import chisel3.util.HasBlackBoxPath
 
 class DMEMIO extends Bundle {
-    val dm_raddr = Input(UInt(64.W))
-    val dm_waddr = Input(UInt(64.W))
+    val addr = Input(UInt(64.W))
     val wdata = Input(UInt(64.W))
     val rdata = Output(UInt(64.W))
 }
@@ -20,8 +19,18 @@ class DMEM extends BlackBox with HasBlackBoxPath {
 }
 
 class DMEM_d extends Module {
-    val io = IO(new DMEMIO)
+    val ALUio = IO(new Bundle {
+        val res_addr = Input(UInt(64.W))
+    })
+    val GPRio = IO(new Bundle {
+        val wdata = Input(UInt(64.W))
+    })
+    val ioMUX = IO(new Bundle {
+        val rdata = Output(UInt(64.W))
+    })
 
     val dmem = Module(new DMEM())
-    dmem.io <> io
+    dmem.io.addr := ALUio.res_addr
+    dmem.io.wdata := GPRio.wdata
+    ioMUX.rdata := dmem.io.rdata
 }
--- npc/vsrc/chisel/DMEM/src/DMEM.v
+++ npc/vsrc/chisel/DMEM/src/DMEM.v
@@ -2,11 +2,10 @@ module DMEM #(
     ADDR_WIDTH = 64,
     DATA_WIDTH = 64
 ) (
-    input   [ADDR_WIDTH-1:0]    dm_raddr,
-    input   [ADDR_WIDTH-1:0]    dm_waddr,
+    input   [ADDR_WIDTH-1:0]    addr,
     input   [DATA_WIDTH-1:0]    wdata,
     output  [DATA_WIDTH-1:0]    rdata
 );
     import "DPI-C" function int paddr_read(input longint unsigned paddr);
-    assign data = paddr_read(dm_raddr);
+    assign data = paddr_read(addr);
 endmodule
