ed07899a1b58a88fc5f7f80af32e743c4f219121
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-10 11:28:13 +0800

    Add input ports in IFU in supporting of J&B inst.

--- npc/vsrc/chisel/IFU/src/IFU.scala
+++ npc/vsrc/chisel/IFU/src/IFU.scala
@@ -6,12 +6,17 @@ class IOIMEM extends Bundle {
     val pc = Output(UInt(64.W))
 }
 
+class MUXIO extends Bundle {
+    val pcNext = Input(UInt(64.W))
+}
+
 class IFU extends Module {
     val ioIMEM = IO(new IOIMEM)
+    val MUXio = IO(new MUXIO)
 
     val pc = RegInit("h80000000".U(64.W)) // Hex Litvalue from string
 
-    pc := pc + 4.U
+    pc := MUXio.pcNext
 
     ioIMEM.pc := pc
 }
