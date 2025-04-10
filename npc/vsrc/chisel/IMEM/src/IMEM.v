fc43afc7d45fb10bfe574a786e9113c48838fe37
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-10 19:10:15 +0800

    Fix IMEM DPI-C function name.

--- npc/vsrc/chisel/IMEM/src/IMEM.v
+++ npc/vsrc/chisel/IMEM/src/IMEM.v
@@ -6,6 +6,6 @@ module IMEM #(
     output  [INST_WIDTH-1:0]    inst_data
     
 );
-    import "DPI-C" function int paddr_read(input longint unsigned paddr);
-    assign inst_data = paddr_read(im_raddr);
+    import "DPI-C" function int pimem_read(input longint unsigned paddr);
+    assign inst_data = pimem_read(pc);
 endmodule
