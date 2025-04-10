4ebf81200814f7ad99f8f661b83c6bf0ecdc3eb5
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-10 11:24:03 +0800

    Add branchOp support and merge ADD and SUB op together.

--- npc/vsrc/chisel/ALU/src/ALU.scala
+++ npc/vsrc/chisel/ALU/src/ALU.scala
@@ -2,32 +2,37 @@ package alu
 
 import chisel3._
 import chisel3.util.MuxLookup
+import chisel3.util.Fill
 
 import aluop._
-import idu.IOALU
+import idu.{IOALU, IOJMP}
+import chisel3.util.Cat
 
 class IODMEMUX extends Bundle {
     val res_addr = Output(UInt(64.W))
 }
 
 class ALU extends Module {
-    val IDUio = IO(Flipped(new IOALU))
-    val GPRio = IO(new Bundle{
-        val data1 = Input(UInt(64.W))
-    })
+    val IDUio = IO(Flipped(new idu.IOALU))
     val MUXio = IO(new Bundle{
+        val data1 = Input(UInt(64.W))
         val data2 = Input(UInt(64.W))
     })
     val ioDMEMUX = IO(new IODMEMUX)
+    val ioJMP = IO(new idu.IOJMP)
+    /* 一：aluop是ADD（jal和jalrn指令）；为高*/
+    /* 二：aluop是SUB（B类型指令）&& 比较结果为真；为高*/
 
-    val addRes = GPRio.data1 + MUXio.data2
-    val subRes = GPRio.data1 - MUXio.data2
-    val xorRes = GPRio.data1 ^ MUXio.data2
-    val orRes  = GPRio.data1 | MUXio.data2
-    val andRes = GPRio.data1 & MUXio.data2
-    val sllRes = GPRio.data1 << MUXio.data2(5,0)
-    val srlRes = GPRio.data1 >> MUXio.data2(5,0)
-    val sraRes = (GPRio.data1.asSInt >> MUXio.data2(5,0)).asUInt
+    val data2s = (MUXio.data2 ^ Fill(64, IDUio.branchsign)) + Cat(0.U(63.W), IDUio.branchsign)
+
+    val addsubRes = MUXio.data1 + data2s
+    // val subRes = GPRio.data1 - MUXio.data2
+    val xorRes = MUXio.data1 ^ MUXio.data2
+    val orRes  = MUXio.data1 | MUXio.data2
+    val andRes = MUXio.data1 & MUXio.data2
+    val sllRes = MUXio.data1 << MUXio.data2(5,0)
+    val srlRes = MUXio.data1 >> MUXio.data2(5,0)
+    val sraRes = (MUXio.data1.asSInt >> MUXio.data2(5,0)).asUInt
     val mulRes = /* IDUio.data1 * IDUio.data2 */ 0.U
     val divRes = /* IDUio.data1 / IDUio.data2 */ 0.U
     val remRes = /* IDUio.data1 % IDUio.data2 */ 0.U
@@ -37,8 +42,8 @@ class ALU extends Module {
 
     ioDMEMUX.res_addr := MuxLookup(aluopDecoded, 0.U)(
         Seq(
-            AluOp.ADD -> addRes,
-            AluOp.SUB -> subRes,
+            AluOp.ADD -> addsubRes,
+            AluOp.SUB -> addsubRes,
             AluOp.XOR -> xorRes,
             AluOp.OR  -> orRes,
             AluOp.AND -> andRes,
@@ -49,4 +54,18 @@ class ALU extends Module {
             AluOp.DIV -> divRes,
             AluOp.REM -> remRes
     ))
+
+    val (branchopDecoded: aluop.BranchOp.Type, validb: Bool) = aluop.BranchOp.safe(IDUio.branchop)
+    assert(validb, "ALU.scala: branchop decode result may be invalid");
+
+    val branchRes = MuxLookup(branchopDecoded, 0.B)(
+        Seq(
+            BranchOp.EQ -> (addsubRes === 0.U),
+            BranchOp.NE -> (addsubRes =/= 0.U),
+            BranchOp.LT -> (Mux(IDUio.branchsign, addsubRes.asSInt < 0.S, addsubRes.asUInt < 0.U)),
+            BranchOp.GE -> (Mux(IDUio.branchsign, addsubRes.asSInt >= 0.S, addsubRes.asUInt >= 0.U))
+        )
+    )
+    ioJMP.pcIncslct := (IDUio.aluop === aluop.AluOp.ADD.litValue.U((aluop.AluOp.getWidth).W)) ||
+                       (IDUio.aluop === aluop.AluOp.SUB.litValue.U((aluop.AluOp.getWidth).W) && branchRes)
 }
