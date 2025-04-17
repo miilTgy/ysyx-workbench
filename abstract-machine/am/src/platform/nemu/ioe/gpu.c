8baf4aa4ff78b82fa7e34e6eeb5320fad3f1243a
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-17 16:33:42 +0800

    Make nemu support video.

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,37 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = (int) w, .height = (int) h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      // pixels[i][j] is color of (i+x, j+y)
+      fb[screen_w*(i+y)+(j+x)] = pixels[w*i+j];
+    }
+  }
+  // fb[0*screen_w + 50] = 0x00ffffff; // Test point
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
