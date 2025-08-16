458562ae2e63fe034ae05ef8eea52881e4668f95
miil <tgy_scut2021@outlook.com>
2025-08-16 16:53:43 +0800

    Add fencei.

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -343,6 +343,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, difftest_skip_ref(); s->dnpc = isa_mret());
 
   INSTPAT("0000 ???? ???? 00000 000 00000 00011 11", fence , N, ); // fence is nop now
+  INSTPAT("0000 0000 0000 00000 001 00000 00011 11", fencei, N, ); // fencei is nop now
   
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
