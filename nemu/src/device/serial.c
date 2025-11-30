f758db5ed9da0520349a53596f288e25fbe5068d
miil <tgy_scut2021@outlook.com>
2025-11-30 15:02:38 +0800

    Debug serial.

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -13,6 +13,9 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "macro.h"
+#include <stdint.h>
+#include <sys/types.h>
 #include <utils.h>
 #include <device/map.h>
 
@@ -27,14 +30,27 @@ static uint8_t *serial_base = NULL;
 static void serial_putc(char ch) {
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
-
+// static uint8_t serial_getc() {
+//   const char *str = "ls\n";
+//   static int i = 0;
+//   if (str[i] != '\0') {
+//     return str[i++];
+//   }
+//   else {
+//     return 0;
+//   }
+// }
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   assert(len == 1);
   switch (offset) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
       if (is_write) serial_putc(serial_base[0]);
-      else panic("do not support read");
+      // else panic("do not support read");
+      else {
+        // serial_base[0] = serial_getc();
+        serial_base[0] = 0;
+      }
       break;
     case 3: // 串口线路控制寄存器 - 忽略配置
     case 5: // 串口状态寄存器 - 返回“就绪”状态
