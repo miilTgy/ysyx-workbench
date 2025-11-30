48365ac0ac07dd20e654ba406ec172f2081132a3
miil <tgy_scut2021@outlook.com>
2025-11-30 15:02:16 +0800

    Add some debug info for mmio.

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -41,6 +41,27 @@ static void check_bound(IOMap *map, paddr_t addr) {
         addr, map->name, map->low, map->high, cpu.pc);
   }
 }
+// is_write: false = read, true = write
+// static bool check_bound(IOMap *map, paddr_t addr, int len, bool is_write) {
+//   if (map == NULL) {
+//     cpu.csr.mtval = addr;
+//     word_t ex = is_write ? 7 : 5;
+//     word_t npc = isa_raise_intr(ex, cpu.pc);
+//     cpu.pc = npc;
+//     return false;
+//   }
+
+//   // 这里顺便把 len 也考虑进去，防止末尾越界
+//   if (addr < map->low || addr + len - 1 > map->high) {
+//     cpu.csr.mtval = addr;
+//     word_t ex = is_write ? 7 : 5;
+//     word_t npc = isa_raise_intr(ex, cpu.pc);
+//     cpu.pc = npc;
+//     return false;
+//   }
+
+//   return true;
+// }
 
 static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write) {
   if (c != NULL) { c(offset, len, is_write); }
@@ -54,7 +75,13 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  // if (addr == 0xfffffee8)
+  //   printf("read\n");
   check_bound(map, addr);
+  //   if (!check_bound(map, addr, len, false)) {
+  //   // 抛了异常，直接返回一个占位值
+  //   return 0;
+  // }
   paddr_t offset = addr - map->low;
   IFDEF(CONFIG_DTRACE, Log("read %d bytes at offset 0x%08x in map "ANSI_FG_RED"%s"ANSI_FG_BLUE, len, offset, map->name);)
   invoke_callback(map->callback, offset, len, false); // prepare data to read
@@ -64,7 +91,13 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  // if (addr == 0xfffffee8)
+  //   printf("write\n");
   check_bound(map, addr);
+  // if (!check_bound(map, addr, len, true)) {
+  //   // 抛了异常，忽略本次写（Linux 会在异常处理里决定下一步）
+  //   return;
+  // }
   paddr_t offset = addr - map->low;
   IFDEF(CONFIG_DTRACE, Log("write %d bytes at offset 0x%08x in map "ANSI_FG_RED"%s"ANSI_FG_BLUE" with data 0x%016lx", len, offset, map->name, data);)
   host_write(map->space + offset, len, data);
